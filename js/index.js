ab9d9737a1dd4b17987d469bd95cdd99dac142c8
kudryashov <jahma123@icloud.com>
2021-06-29 17:37:21 +0300

    replace link with localpaths

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ window.onload = () => {
     const scene = new THREE.Scene();
 
     const sun_loader = new THREE.TextureLoader();
-    const sun_texture = sun_loader.load('https://avatars.mds.yandex.net/get-images-cbir/3776399/X0Kej21mbBm4oBTuhbN1DA4224/ocr');
+    const sun_texture = sun_loader.load('./img/sun.jpg');
     sun_texture.anisotropy = 16;
 
     const sun_geometry = new THREE.SphereGeometry(2300, 80, 80);
@@ -48,12 +48,12 @@ window.onload = () => {
         return obj;
     }
 
-    const earth = createSphere([100, 40, 40], 7500, 'https://avatars.mds.yandex.net/get-images-cbir/4027797/6IQhm60wtfWE5fiQ5KxUXw2214/ocr');
-    const mercury = createSphere([60, 20, 20], 4000, 'https://avatars.mds.yandex.net/get-images-cbir/4509884/K_arIdh1KM2gk3k25pAJRg2511/ocr');
-    const venus = createSphere([90, 20, 20], 5500,'https://avatars.mds.yandex.net/get-images-cbir/4576821/5GTZ2aUaCci8L8y92q9t2Q9656/ocr');
-    const mars = createSphere([80, 20, 20], 8000, 'https://avatars.mds.yandex.net/get-images-cbir/4330622/ij5v83NbBTtp-ctdP6k2zw1454/ocr');
-    const jupiter = createSphere([350, 20, 20], 10700, 'https://avatars.mds.yandex.net/get-images-cbir/4549353/il4xziJs5o9vf47UcegWrw9975/ocr');
-    const saturn = createSphere([230, 20, 20], 12000, 'https://avatars.mds.yandex.net/get-images-cbir/4303061/ohpCpE9yJCa21CmiivNMjw3903/ocr');
+    const earth = createSphere([100, 40, 40], 7500, './img/earth.jpg');
+    const mercury = createSphere([60, 20, 20], 4000, './img/mercury.jpg');
+    const venus = createSphere([90, 20, 20], 5500,'./img/venus.jpg');
+    const mars = createSphere([80, 20, 20], 8000, './img/mars.jpg');
+    const jupiter = createSphere([350, 20, 20], 10700, './img/jupiter.jpg');
+    const saturn = createSphere([230, 20, 20], 12000, './img/saturn.jpg');
 
     const light = new THREE.AmbientLight(0xffffff, 1);
     scene.add(light);
